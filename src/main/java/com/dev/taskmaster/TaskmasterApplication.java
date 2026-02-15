fa061f913453cd7532a9df51677c96153ada582b
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-02-15 20:51:17 +0100

    H2 Confing and POM fixed

--- src/main/java/com/dev/taskmaster/TaskmasterApplication.java
+++ src/main/java/com/dev/taskmaster/TaskmasterApplication.java
@@ -3,7 +3,7 @@ package com.dev.taskmaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dev.taskmaster")
 public class TaskmasterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dev/taskmaster/web/controller/TaskController.java
+++ src/main/java/com/dev/taskmaster/web/controller/TaskController.java
@@ -5,24 +5,35 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dev.taskmaster.domain.model.Task;
 import com.dev.taskmaster.service.TaskService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
+@RequestMapping("/api/tasks")
 @RequiredArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
 
-    @GetMapping("/api/tasks")
+    @GetMapping
     public ResponseEntity<List<Task>> getAllTasks() {
         List<Task> tasks = taskService.getAllTasks();
         return ResponseEntity.ok(tasks);
+    }
+
+    @PostMapping   
+    public ResponseEntity<Task> createTask(@Valid @RequestBody Task task) {
+        Task new_task = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new_task);
     }
     
     
